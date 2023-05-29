bf0bf17ef0f075e9a549528cb3263c2552527efa
Adarsh-Kumbar-in <adarshkumbarbgm@gmail.com>
2023-05-29 22:37:40 +0530

    commit

--- Arrays/Binary Search/4_FindPivotEmtFromSortedandRotatedArray.cpp.cpp
+++ Arrays/Binary Search/4_FindPivotEmtFromSortedandRotatedArray.cpp.cpp
@@ -1,18 +1,19 @@
 // Points......
-// 1.Arr.Length() >=3   
-// 2. 0 to i                Arr[i]<Arr[i+1]
-// 3. i to size             Arr[i]>Arr[i+1]
-// 4. return peak index ... Arr[i]<Arr[i+1] < peak >Arr[i]>Arr[i+1] .. greater than both 2 and 3 pts
-// the Array looks like a mountain
+//1.Pivot any number/index of array used to perform operations in ARRAY
+//2.Pivot we considered smallest value
+//3.Array considered it Sorted and rotated
+// ex: [3,8,10,17,1] o/p pivot index is 4
 #include<iostream>
 using namespace std;
 
 int MountainArray(int Arr[],int size){
-    int start = 0, end = size-1;
+
+    int start = 0;
+    int end = size-1;
     int mid = start + (end - start)/2;
 
     while(start < end ){
-        if(Arr[mid] < Arr[mid+1])
+        if(Arr[mid] >= Arr[0])
         {
             start = mid + 1;// getting closer to peak from left
         }
@@ -24,9 +25,9 @@ int MountainArray(int Arr[],int size){
     return start  ;
 }
 int main (){
-
-    int Arr[] = { 0 , 10 , 50 , 2};     // o/p 2 
-    // int Arr[] = { 0 , 2 , 1 , 0 };  // o/p 1 
-    int peak = MountainArray(Arr,4);
+    //Array should be sorted and rotated
+    // int Arr[] = { 0 , 10 , 50 , 2};     // o/p 3 
+    int Arr[] = { 0,2,4,5,4,3,1 };  // o/p 6 (Arr,7)
+    int peak = MountainArray(Arr,7);
     cout<< "Peak index  is : "<< peak << endl;
 }
