f71b93c32702b43099101848e35d21cbfadbdf90
Adarsh-Kumbar-in <adarshkumbarbgm@gmail.com>
2023-08-10 00:31:45 +0530

    POTD

---
+++ POTD/20_LargestPrimeFactor.cpp
@@ -0,0 +1,21 @@
+//                      Largest prime factor
+
+/*
+Given a number N, the task is to find the largest prime factor of that number.
+ Example 1:
+
+Input:
+N = 5
+Output:
+5
+Explanation:
+5 has 1 prime factor i.e 5 only.
+Example 2:
+
+Input:
+N = 24
+Output:
+3
+Explanation:
+24 has 2 prime factors 2 and 3 in which 3 is greater.
+*/
