9736589683cdc89d5c7ff8522b96631f45d83e0f
Adarsh-Kumbar-in <adarshkumbarbgm@gmail.com>
2023-08-30 14:48:47 +0530

    POTD

---
+++ POTD/32_DeleteANodeInSingleLinkedList.cpp
@@ -0,0 +1,128 @@
+//                              Delete a Node in Single Linked List
+// easy
+/*
+Given a singly linked list and an integer x.Delete xth node from the singly linked list.
+
+Example 1:
+
+Input: 1 -> 3 -> 4 
+       x = 3
+Output: 1 -> 3
+Explanation:
+After deleting the node at 3rd
+position (1-base indexing), the
+linked list is as 1 -> 3. 
+Example 2:
+
+Input: 1 -> 5 -> 2 -> 9 
+x = 2
+Output: 1 -> 2 -> 9
+Explanation: 
+After deleting the node at 2nd
+position (1-based indexing), the
+linked list is as 1 -> 2 -> 9.
+*/
+//{ Driver Code Starts
+// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+void append(struct Node** head_ref, struct Node **tail_ref,
+           int new_data)
+{
+    struct Node* new_node = new Node(new_data);
+    
+    if (*head_ref == NULL)
+       *head_ref = new_node;
+    else
+       (*tail_ref)->next = new_node;
+    *tail_ref = new_node;
+}
+
+/* Function to get the middle of the linked list*/
+struct Node* deleteNode(struct Node *head,int );
+
+void printList(Node *head)
+{
+   while (head != NULL)
+   {
+      cout << head->data << " ";
+      head = head->next;
+   }
+   cout << "\n";
+}
+
+/* Driver program to test above function*/
+int main()
+{
+  int T, i, n, l;
+
+    cin>>T;
+
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n;
+        for(i=1;i<=n;i++)
+        {
+            cin>>l;
+            append(&head, &tail, l);
+        }
+
+	int kk;
+	cin>>kk;
+    head = deleteNode(head,kk);
+    printList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+/* Link list Node 
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+/*You are required to complete below method*/
+Node* deleteNode(Node *head,int x)
+{
+    Node * temp = head;
+    if( head == NULL)
+        return head;
+    if( x == 1)
+    {
+        head = head->next;
+        return head;
+    }
+    for(int i = 1 ; i < x - 1; i++){
+        temp = temp->next;
+    }
+    Node * del = temp->next;
+    temp->next = del->next;
+    free(del);
+    return head;
+}
