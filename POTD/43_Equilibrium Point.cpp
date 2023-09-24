a1c116b75d72ab6237de91f86bbe024b0a928d3b
Adarsh Kumbar <adarshkumbarbgm@gmail.com>
2023-09-24 15:25:15 +0530

    POTD

---
+++ POTD/43_Equilibrium Point.cpp
@@ -0,0 +1,85 @@
+//                  Equilibrium Point (easy)
+/*
+Given an array A of n positive numbers. The task is to find the first equilibrium point in an array. Equilibrium point in an array is a position such that the sum of elements before it is equal to the sum of elements after it.
+
+Note: Return equilibrium point in 1-based indexing. Return -1 if no such point exists. 
+
+Example 1:
+
+Input: 
+n = 5 
+A[] = {1,3,5,2,2} 
+Output: 
+3 
+Explanation:  
+equilibrium point is at position 3 as sum of elements before it (1+3) = sum of elements after it (2+2). 
+Example 2:
+
+Input:
+n = 1
+A[] = {1}
+Output: 
+1
+Explanation:
+Since there's only element hence its only the equilibrium point.
+*/
+//{ Driver Code Starts
+#include <iostream>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // Function to find equilibrium point in the array.
+    // a: input array
+    // n: size of array
+    int equilibriumPoint(long long a[], int n) {
+    
+        int totalSum = 0 ;
+        //  calculating total sum of Arr
+        for( int i = 0 ; i < n ; i++){
+            totalSum += a[i];
+        }
+        int prevSum = 0 ; // sum before the index
+        for(int i = 0 ; i < n ; i ++){
+            if( prevSum  == totalSum - prevSum - a[i] )  // to get sum of right part of index
+                return i + 1;
+            prevSum += a[i];
+        }
+        return -1;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+
+int main() {
+
+    long long t;
+    
+    //taking testcases
+    cin >> t;
+
+    while (t--) {
+        long long n;
+        
+        //taking input n
+        cin >> n;
+        long long a[n];
+
+        //adding elements to the array
+        for (long long i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        
+        Solution ob;
+
+        //calling equilibriumPoint() function
+        cout << ob.equilibriumPoint(a, n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
---
+++ POTD/44_FindDuplicatesInAnArray.cpp
@@ -0,0 +1,68 @@
+//              Find duplicates in an array
+/*
+Given an array a of size N which contains elements from 0 to N-1, you need to find all the elements occurring more than once in the given array. Return the answer in ascending order. If no such element is found, return list containing [-1]. 
+
+Note: The extra space is only for the array to be returned. Try and perform all operations within the provided array. 
+
+Example 1:
+
+Input:
+N = 4
+a[] = {0,3,1,2}
+Output: 
+-1
+Explanation: 
+There is no repeating element in the array. Therefore output is -1.
+Example 2:
+
+Input:
+N = 5
+a[] = {2,3,1,2,3}
+Output: 
+2 3 
+Explanation: 
+2 and 3 occur more than once in the given array.
+*/
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+  public:
+    vector<int> duplicates(int arr[], int n) {
+        vector<int> ans;
+        int base = n ; 
+        for(int i = 0 ; i < n ; i++){
+            int num = arr[i] % base;
+            arr[num] = arr[num] + base;
+        }
+        for( int i = 0 ; i < n ; i++){
+            if( arr[i]/ base > 1)
+                ans.push_back(i);
+        }
+        if( ans.size() == 0 )
+            ans.push_back(-1);
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t-- > 0) {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+        Solution obj;
+        vector<int> ans = obj.duplicates(a, n);
+        for (int i : ans) cout << i << ' ';
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
